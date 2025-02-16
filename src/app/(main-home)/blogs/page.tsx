1ed7ca55035ad1e62b36b0b856218b7c69a6924d
Md Sujon Mia <sujon258549@gmail.com>
2025-02-16 20:53:02 +0600

    modified 3 files

--- src/app/(main-home)/blogs/page.tsx
+++ src/app/(main-home)/blogs/page.tsx
@@ -1,4 +1,13 @@
-const BlogsPage = () => {
+const BlogsPage = async () => {
+  //   const res = await fetch("http://localhost:5000");
+
+  //   if (!res.ok) {
+  //     console.error("Failed to fetch blogs:", res.status);
+  //     return;
+  //   }
+
+  //   const data = res.json();
+  //   console.log(data);
   return <div>BlogsPage</div>;
 };
 
